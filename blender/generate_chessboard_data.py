b26bf6789efc0f4de6c3a8fd2a1d5981cce873a2
Tsogtgerel <ts.r.madrid@gmail.com>
2024-04-10 23:21:10 +0800

    fix generator for changed chess character models.

--- blender/generate_chessboard_data.py
+++ blender/generate_chessboard_data.py
@@ -24,8 +24,8 @@ class BlenderChess:
         self.CROP_HEIGHT= 900
         self.IMG_WIDTH = 640
         self.IMG_HEIGHT = 640
-        self.TRAIN_ITER = 400
-        self.VAL_ITER = 20
+        self.TRAIN_ITER = 24000
+        self.VAL_ITER = 600
         self.TEST_ITER = 10
 
         # world = bpy.data.worlds['World']
@@ -42,7 +42,24 @@ class BlenderChess:
         bg_img = random.choice(bg_imgs)
         self.bg_node.image = bpy.data.images.load(os.path.join(self.blender_dir,"..", "backgrounds", bg_img))
 
-    def set_material_to_current_object(self, material_name):
+    def set_material_to_current_object(self, obj, material_name):
+        if obj is None:
+            print(f"Object not found.")
+            return
+        
+        # Get the material by name
+        mat = bpy.data.materials.get(material_name)
+        if mat is None:
+            print(f"Material '{material_name}' not found.")
+            return
+        
+        # Assign the material to the object
+        if obj.data.materials:
+            # If the object already has materials assigned, replace the first one
+            obj.data.materials[0] = mat
+        else:
+            # Otherwise, add the material to the object
+            obj.data.materials.append(mat)
         # Get the material
         material = bpy.data.materials.get(material_name)
 
@@ -52,7 +69,8 @@ class BlenderChess:
             return
 
         # Assign the material to the active object
-        bpy.context.object.data.materials.append(material)
+        
+        #bpy.context.object.data.materials.append(material)
         
 
     def copy_model_by_name(self, model_index, color, collection_name, y_pos, x_pos):
@@ -65,10 +83,9 @@ class BlenderChess:
         # Get the duplicated object
         duplicated_obj = bpy.context.active_object
         if color==self.BLACK:
-            self.set_material_to_current_object("BlackModel")
+            self.set_material_to_current_object(duplicated_obj, "BlackModel")
         else:
-            self.set_material_to_current_object("WhiteModel")
-        self.set_material_to_current_object("BlackModel")
+            self.set_material_to_current_object(duplicated_obj, "WhiteModel")
 
         # Create collection if it doesn't exist
         if collection_name not in bpy.data.collections:
@@ -139,52 +156,64 @@ class BlenderChess:
         for i in range(total_models):
             model_index = random.randint(0, len(self.models)-1)
             side = random.choice([self.BLACK, self.WHITE])
+            bpy.data.objects["Board"].hide_render = random.choice([True, False])
+
             x_pos, y_pos = self.get_random_not_collided_position(data, 1.5 * self.MODEL_RADIUS)
             self.copy_model_by_name(model_index, side, "models_temp", x_pos, y_pos)
             data.append([f"{model_index + side * len(self.models)}", side, x_pos, y_pos])
 
-        # self.copy_model_by_name(0, 0, "models_temp", self.MAX_X, -self.MAX_Y)
-        # data.append([0,0, self.MAX_X, -self.MAX_Y])
-        # self.copy_model_by_name(0, 0, "models_temp", -self.MAX_X, -self.MAX_Y)
-        # data.append([0,0, -self.MAX_X, -self.MAX_Y])
-        # self.copy_model_by_name(0, 0, "models_temp", self.MAX_X, -self.MAX_Y/2)
-        # data.append([0,0, self.MAX_X, -self.MAX_Y/2])
-        # self.copy_model_by_name(0, 0, "models_temp", -self.MAX_X, -self.MAX_Y/2)
-        # data.append([0,0, -self.MAX_X, -self.MAX_Y/2])
-        # self.copy_model_by_name(0, 0, "models_temp", self.MAX_X, 0)
-        # data.append([0,0, self.MAX_X, 0])
-        # self.copy_model_by_name(0, 0, "models_temp", -self.MAX_X, 0)
-        # data.append([0,0, -self.MAX_X, 0])
-        # self.copy_model_by_name(0, 0, "models_temp", self.MAX_X, self.MAX_Y)
-        # data.append([0,0, self.MAX_X, self.MAX_Y])
-        # self.copy_model_by_name(0, 0, "models_temp", -self.MAX_X, self.MAX_Y)
-        # data.append([0,0, -self.MAX_X, self.MAX_Y])
-        # self.copy_model_by_name(0, 0, "models_temp", self.MAX_X, self.MAX_Y/2)
-        # data.append([0,0, self.MAX_X, self.MAX_Y/2])
-        # self.copy_model_by_name(0, 0, "models_temp", -self.MAX_X, self.MAX_Y/2)
-        # data.append([0,0, -self.MAX_X, self.MAX_Y/2])
-        # self.copy_model_by_name(0, 0, "models_temp", 0, -self.MAX_Y)
-        # data.append([0,0, 0, -self.MAX_Y])
-        # self.copy_model_by_name(0, 0, "models_temp", 0, self.MAX_Y)
-        # data.append([0,0, 0, self.MAX_Y])
+        # self.copy_model_by_name(2, 0, "models_temp", self.MAX_X, -self.MAX_Y)
+        # data.append([2,0, self.MAX_X, -self.MAX_Y])
+        # self.copy_model_by_name(2, 0, "models_temp", -self.MAX_X, -self.MAX_Y)
+        # data.append([2,0, -self.MAX_X, -self.MAX_Y])
+        # self.copy_model_by_name(2, 0, "models_temp", self.MAX_X, -self.MAX_Y/2)
+        # data.append([2,0, self.MAX_X, -self.MAX_Y/2])
+        # self.copy_model_by_name(2, 0, "models_temp", -self.MAX_X, -self.MAX_Y/2)
+        # data.append([2,0, -self.MAX_X, -self.MAX_Y/2])
+        # self.copy_model_by_name(2, 0, "models_temp", self.MAX_X, 0)
+        # data.append([2,0, self.MAX_X, 0])
+        # self.copy_model_by_name(2, 0, "models_temp", -self.MAX_X, 0)
+        # data.append([2,0, -self.MAX_X, 0])
+        # self.copy_model_by_name(2, 0, "models_temp", self.MAX_X, self.MAX_Y)
+        # data.append([2,0, self.MAX_X, self.MAX_Y])
+        # self.copy_model_by_name(2, 0, "models_temp", -self.MAX_X, self.MAX_Y)
+        # data.append([2,0, -self.MAX_X, self.MAX_Y])
+        # self.copy_model_by_name(2, 0, "models_temp", self.MAX_X, self.MAX_Y/2)
+        # data.append([2,0, self.MAX_X, self.MAX_Y/2])
+        # self.copy_model_by_name(2, 0, "models_temp", -self.MAX_X, self.MAX_Y/2)
+        # data.append([2,0, -self.MAX_X, self.MAX_Y/2])
+        # self.copy_model_by_name(2, 0, "models_temp", 0, -self.MAX_Y)
+        # data.append([2,0, 0, -self.MAX_Y])
+        # self.copy_model_by_name(2, 0, "models_temp", 0, self.MAX_Y)
+        # data.append([2,0, 0, self.MAX_Y])
         return  data
 
 
     def calculate_label(self, row):
        
         scale=1
-        if row[0]!=0 and row[0]!=6: # not pawn
-            scale=1.3
+        if row[0]==0 and row[0]==6: # pawn
+            scale=1
+        elif row[0]==1 and row[0]==7: # bishop
+            scale=1.75
+        elif row[0]==2 and row[0]==8: # king 
+            scale=1.9
+        elif row[0]==3 and row[0]==9: # queen
+            scale=1.8
+        elif row[0]==4 and row[0]==10: # rook
+            scale=1.5
+        elif row[0]==5 and row[0]==11: # knight
+            scale=1.7
         
         trapets_ind_x = 1 + (row[3] / 8.6)
-        
+        scale = scale * (1.5 - (row[3] / 5 ))
         trapets_ind_y = 1
 
-        y = (row[3] * trapets_ind_x) / (2.6 * self.MAX_Y) + 0.465 * self.MAX_Y 
+        y = (row[3] * trapets_ind_x) / (2.5 * self.MAX_Y) + 0.440 * self.MAX_Y
         print(row[3], y)
-        x = (row[2] * trapets_ind_x) / (2.4 * self.MAX_X)  + 0.490 * self.MAX_X
+        x = (row[2] * trapets_ind_x) / (2.3 * self.MAX_X)  + 0.490 * self.MAX_X
         print(row[2], x)
-        height = scale * self.MODEL_RADIUS  / (1.7 * self.MAX_Y * trapets_ind_x)           
+        height = (scale) * self.MODEL_RADIUS  / (2.4     * self.MAX_Y * trapets_ind_x) + 0.04
         width = scale * self.MODEL_RADIUS * trapets_ind_x / (2.4 * self.MAX_X) + abs(row[2])/50
         if x < 0 or x > 1 or y < 0 or y > 1:
             print("Invalid label", row, x, y, width, height)
@@ -212,6 +241,6 @@ class BlenderChess:
 
 
 blender_chess = BlenderChess()
-blender_chess.generate_data("train", blender_chess.TRAIN_ITER, 0)
-blender_chess.generate_data("val", blender_chess.VAL_ITER, 0)
-blender_chess.generate_data("test", blender_chess.TEST_ITER, 0)
+#blender_chess.generate_data("test",blender_chess.TEST_ITER, 0)
+blender_chess.generate_data("val", blender_chess.VAL_ITER, 220)
+blender_chess.generate_data("train", blender_chess.TRAIN_ITER, 1610)
--- predict.py
+++ predict.py
@@ -4,8 +4,8 @@ import argparse
 # Initialize model
 
 parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--image", required=True, help="Image file to predict")
-parser.add_argument("--weight", required=False, default="runs/detect/train/weights/best.pt", help="Image file to predict")
+parser.add_argument("--image", required=False,  default="datasets/test/images/", help="Image file or directory to predict")
+parser.add_argument("--weight", required=False, default="runs/detect/train/weights/best.pt", help="Weight of the training model")
 args = parser.parse_args()
 
 
