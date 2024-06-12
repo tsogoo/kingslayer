7c53b73ae56bb0b17a333d8dd48bf19228c142fb
ErdeneL <erdene.lu.n@gmail.com>
2024-06-12 22:11:30 +0800

    test

--- chess_helper.py
+++ chess_helper.py
@@ -70,52 +70,52 @@ def main():
         print("Game Over")
         print("Result: ", board.result())
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 # example
-class Test:
+# class Test:
 
-    def __init__(self):
+#     def __init__(self):
         
-        import os
-        import yaml
+#         import os
+#         import yaml
 
-        current_file_path = os.path.abspath(__file__)
-        parent_directory = os.path.dirname(current_file_path)
-        with open(os.path.join(parent_directory, 'app.yaml'), 'r') as file:
-            config = yaml.safe_load(file)
+#         current_file_path = os.path.abspath(__file__)
+#         parent_directory = os.path.dirname(current_file_path)
+#         with open(os.path.join(parent_directory, 'app.yaml'), 'r') as file:
+#             config = yaml.safe_load(file)
 
-        from engine.helper import ChessEngineHelper
-        from robot_arm.robot import Robot
-        self.e = ChessEngineHelper()
-        self.robot = Robot(config=config)
+#         from engine.helper import ChessEngineHelper
+#         from robot_arm.robot import Robot
+#         self.e = ChessEngineHelper()
+#         self.robot = Robot(config=config)
 
-    def get_figure_actual_position(self, x, y, is_occupied=False):
-        # new board square height, width = 370/8 = 46.25
-        # x, y => figure's square coordinate on board, is_occupied => whether square is occupied
-        if is_occupied:
-            # TODO get detected location
-            return x*46+23, y*46+23
-        # center of square
-        return x*46+23, y*46+23
+#     def get_figure_actual_position(self, x, y, is_occupied=False):
+#         # new board square height, width = 370/8 = 46.25
+#         # x, y => figure's square coordinate on board, is_occupied => whether square is occupied
+#         if is_occupied:
+#             # TODO get detected location
+#             return x*46+23, y*46+23
+#         # center of square
+#         return x*46+23, y*46+23
     
-    def test(self):
-        e = self.e
-        e.initialize_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") # new game
-        # e.initialize_board("4k3/P7/8/8/8/8/8/4K3 w") # promotion
-        # e.initialize_board("4k3/8/8/8/8/8/p7/4K3 b") # promotion
-        # e.initialize_board("4k2r/p7/8/8/8/8/8/4K3 b KQkq") # castling
-        # e.initialize_board("r3k3/8/8/8/8/8/8/4K3 b KQkq") # castling
-        while not e.board.is_game_over():
-            # print(e.board)
-            m = e.get_best_move()
-            if e.is_valid_move(m):
-                self.robot.move(e, self, m)
-                e.move(m)
-                p = input("type_move/e7e3/:")
-                if e.is_valid_move(p):
-                    e.move(p)
-                print(e.board)
-t = Test()
-t.test()
+#     def test(self):
+#         e = self.e
+#         e.initialize_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") # new game
+#         # e.initialize_board("4k3/P7/8/8/8/8/8/4K3 w") # promotion
+#         # e.initialize_board("4k3/8/8/8/8/8/p7/4K3 b") # promotion
+#         # e.initialize_board("4k2r/p7/8/8/8/8/8/4K3 b KQkq") # castling
+#         # e.initialize_board("r3k3/8/8/8/8/8/8/4K3 b KQkq") # castling
+#         while not e.board.is_game_over():
+#             # print(e.board)
+#             m = e.get_best_move()
+#             if e.is_valid_move(m):
+#                 self.robot.move(e, self, m)
+#                 e.move(m)
+#                 p = input("type_move/e7e3/:")
+#                 if e.is_valid_move(p):
+#                     e.move(p)
+#                 print(e.board)
+# t = Test()
+# t.test()
